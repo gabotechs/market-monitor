6d7e5334c627fd09dff194615a0dbcd3a91ae436
gabriel <gabimtme@gmail.com>
2022-05-19 07:15:06 +0200

    feat: sentiment analysis on tweets

--- src/metric_collectors/twitter_tweet_collector.py
+++ src/metric_collectors/twitter_tweet_collector.py
@@ -1,7 +1,7 @@
 from typing import List
 import time
 import logging
-
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from metric_publisher.metric_collector import MetricCollector, MetricEntry
 from twitter_api import TwitterApi
 
@@ -10,6 +10,7 @@ class TwitterTweetCollector(MetricCollector):
     MEASURE_NAME = "twitter"
 
     def __init__(self, token: str, logger: logging.Logger):
+        self.analyzer = SentimentIntensityAnalyzer()
         self.logger = logger
         self.api = TwitterApi(token, logger)
 
@@ -18,9 +19,10 @@ class TwitterTweetCollector(MetricCollector):
         metrics: List[MetricEntry] = []
         for new_tweet in new_tweets:
             time_ns = int(time.mktime(new_tweet.timestamp.timetuple())*1e9)
+            sentiment = self.analyzer.polarity_scores(new_tweet.content)
             metrics.append(MetricEntry(
                 measure_name=self.MEASURE_NAME,
-                data=new_tweet.__dict__,
+                data={**new_tweet.__dict__, "sentiment": sentiment},
                 time=time_ns
             ))
         return metrics
